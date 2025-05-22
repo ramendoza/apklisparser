987cd7df993c0b46b22ab48b189c18d72f269b31
Rafa <ralexm14@gmail.com>
2025-05-22 10:01:52 -0400

    fix. update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="apklisparser",
-    version="1.0.3",
+    version="1.1.0",
     packages=["apklisparser"],
     author="Rafael Mendoza",
     author_email="ralexm14@gmail.com",
